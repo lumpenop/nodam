cd2ea2c14bc56dce2ce4d922ba00280dcf7a480b
lumpenop <swk5373@gmail.com>
2023-05-25 03:15:59 +0900

    feat: main tab 을 material bottom tab 으로 변경

--- src/navigations/main.tab.tsx
+++ src/navigations/main.tab.tsx
@@ -1,50 +1,52 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 import Home from 'src/screens/Home/home';
 import MyPage from 'src/screens/MyPage/my.page';
 import Record from 'src/screens/Record/record';
 import Tips from 'src/screens/Tips/tips';
+import HomeIcon from 'src/assets/svgs/home.svg';
+import CalendarIcon from 'src/assets/svgs/calendar.svg';
+import CoffeeIcon from 'src/assets/svgs/coffee.svg';
+import GithubIcon from 'src/assets/svgs/github.svg';
 
 const MainTab = () => {
-  const Tab = createBottomTabNavigator<MainTabParamsType>();
+  const Tab = createMaterialBottomTabNavigator<MainTabParamsType>();
+  const size = 30;
   return (
     <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#6200ee',
-      }}>
+      initialRouteName="HomeStack"
+      barStyle={{backgroundColor: 'tomato'}}>
       <Tab.Screen
         name={'HomeStack'}
         component={Home}
         options={{
-          tabBarIcon: ({color}) => <Text style={{color}}>Home</Text>,
-          headerShown: false,
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => <HomeIcon color={color} width={size} />,
         }}
       />
       <Tab.Screen
         name={'RecordStack'}
         component={Record}
         options={{
-          tabBarIcon: ({color}) => <Text style={{color}}>Record</Text>,
-          headerShown: false,
+          tabBarLabel: 'Record',
+          tabBarIcon: ({color}) => <CalendarIcon color={color} width={size} />,
         }}
       />
       <Tab.Screen
         name={'TipsStack'}
         component={Tips}
         options={{
-          tabBarIcon: ({color}) => <Text style={{color}}>Record</Text>,
-          headerShown: false,
+          tabBarLabel: 'Tips',
+          tabBarIcon: ({color}) => <CoffeeIcon color={color} width={size} />,
         }}
       />
       <Tab.Screen
         name={'MyPageStack'}
         component={MyPage}
         options={{
-          tabBarIcon: ({color}) => <Text style={{color}}>Record</Text>,
-          headerShown: false,
+          tabBarLabel: 'My',
+          tabBarIcon: ({color}) => <GithubIcon color={color} width={size} />,
         }}
       />
     </Tab.Navigator>
