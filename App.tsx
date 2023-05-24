388d3f1ed68e25afba34b61fcf8de8c8ac487479
lumpenop <swk5373@gmail.com>
2023-05-25 03:15:21 +0900

    feat: main theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {Provider as JotaiProvider} from 'jotai';
 import RootStack from 'src/navigations/root.stack';
 
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(255, 45, 85)',
+    background: '#2A2C40',
+  },
+};
+
 const App = () => {
   return (
     <JotaiProvider>
-      <NavigationContainer>
+      <NavigationContainer theme={MyTheme}>
         <StatusBar hidden />
         <RootStack />
       </NavigationContainer>
